8d975472cf555009b98131561061f268d477ce81
maximiliano.roldan@astropay.com <maximiliano.roldan@astropay.com>
2025-02-01 20:52:03 -0300

    Add home card with version

--- app/src/main/java/com/estaciondulce/app/MyApp.kt
+++ app/src/main/java/com/estaciondulce/app/MyApp.kt
@@ -8,8 +8,7 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         try {
-            val options = FirebaseApp.initializeApp(this)?.options
-            Log.d("FirebaseConfig", "Firebase initialized with Project ID: ${options?.projectId}, App ID: ${options?.applicationId}")
+            FirebaseApp.initializeApp(this)?.options
         } catch (e: Exception) {
             Log.e("FirebaseConfig", "Error initializing Firebase: ${e.message}")
         }
